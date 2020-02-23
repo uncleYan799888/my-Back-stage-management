5309314fa09e520439f554adb4e1f07986a87a53
uncleYan799888 <18819534330@163.com>
2020-02-23 20:59:07 +0800

    优化

--- src/views/login.vue
+++ src/views/login.vue
@@ -42,10 +42,11 @@ export default {
       console.log('login')
       axios.post('http://localhost:3000/login',{account: this.userInfo.account, password: this.userInfo.password}).then(res => {
         // this.$store.dispatch('loginByUsername',this.userInfo).then(() =>{
-          if(res.statu === 1) {
+          console.log('login-res')
+          if(res.data.statu === 1) {
             console.log('登录成功', res.token)
-            window.sessionStorage.setItem('token', res.token)
-            this.login_in(res.token)
+            window.sessionStorage.setItem('token', res.data.token)
+            this.login_in(res.data.token)
                 // setTimeout(() => {
                   this.$router.push({path: '/base'})
                 // }, 500);
